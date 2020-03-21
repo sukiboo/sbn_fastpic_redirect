564fc1a5277b56d4c41395bde85708d7756942a1
Anton Dereventsov <dereventsov@gmail.com>
2020-03-21 18:44:51 -0400

    version 0.4

--- fastpic_redirect.js
+++ fastpic_redirect.js
@@ -15,20 +15,29 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 // check if host is 'fastpic.ru' and url ends with '.html'
 chrome.webNavigation.onCommitted.addListener(function(obj) {
-	// generate the link to full image
+	// parse the current url
 	var s = obj.url.split('/');
+	console.log('s = ', s)
+	// generate the link to full image
+	// old redirect, might not work anymore but leaving just in case
 	var ss = s[7].split('.');
 	var new_url = [s[0], s[1], 'i'+s[4]+'.'+s[2], 'big', s[5], s[6], ss[0].slice(-2), ss[0]+'.'+ss[1]].join('/');
+	// UPD 03.2020: change 'view' to 'fullview' in url
+	var full_url = [s[0], s[1], s[2], 'fullview', s[4], s[5], s[6], ss[0] + '.' + ss[1]].join('/');
+	console.log('full_url = ', full_url)
+
 	// start redirecting if status allows and add '?noht=1' flag if needed
 	chrome.webNavigation.onDOMContentLoaded.addListener(function(tab) {
 		if (state) {
-			if (tab.url.endsWith('?noht=1')) {
-				console.log('sbn: redirecting', tab.url, 'to', new_url);
-				chrome.tabs.update(tab.tabId, {url: new_url});
-			} else {
-				console.log('sbn: redirecting', tab.url, 'to', new_url+'?noht=1');
-				chrome.tabs.update(tab.tabId, {url: new_url+'?noht=1'});
-			}
+			console.log('sbn: redirecting', tab.url, 'to', full_url);
+			chrome.tabs.update(tab.tabId, {url: full_url});
+//			if (tab.url.endsWith('?noht=1')) {
+//				console.log('sbn: redirecting', tab.url, 'to', new_url);
+//				chrome.tabs.update(tab.tabId, {url: new_url});
+//			} else {
+//				console.log('sbn: redirecting', tab.url, 'to', new_url+'?noht=1');
+//				chrome.tabs.update(tab.tabId, {url: new_url+'?noht=1'});
+//			}
 		}
-	}, {hostEquals: 'fastpic.ru', url: [{pathSuffix: s[7]}]});;
+	}, {hostEquals: 'fastpic.ru', url: [{pathSuffix: s[7]}]});
 }, {url: [{hostEquals: 'fastpic.ru', urlSuffix: '.html'}]});
