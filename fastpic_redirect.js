7201668808b2478ab5409e20d23a2cb8bfbe3273
Anton Dereventsov <dereventsov@gmail.com>
2020-04-18 14:53:22 -0400

    refactoring

--- fastpic_redirect.js
+++ fastpic_redirect.js
@@ -1,4 +1,8 @@
-'use strict';
+// this extension skips advertisements on fastpic.ru and shows the full image
+// upd: since the beginning of 2020 fastpic.ru have changed the path to files
+// so now to obtain the link to the full image one needs simply to edit
+// the url by replacing 'view' to 'fullview' and removing the '.html' suffix
+
 
 // switch extension status by icon click
 var state = 1;
@@ -13,31 +17,22 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	}
 });
 
-// check if host is 'fastpic.ru' and url ends with '.html'
-chrome.webNavigation.onCommitted.addListener(function(obj) {
-	// parse the current url
-	var s = obj.url.split('/');
-	console.log('s = ', s)
-	// generate the link to full image
-	// old redirect, might not work anymore but leaving just in case
-	var ss = s[7].split('.');
-	var new_url = [s[0], s[1], 'i'+s[4]+'.'+s[2], 'big', s[5], s[6], ss[0].slice(-2), ss[0]+'.'+ss[1]].join('/');
-	// UPD 03.2020: change 'view' to 'fullview' in url
-	var full_url = [s[0], s[1], s[2], 'fullview', s[4], s[5], s[6], ss[0] + '.' + ss[1]].join('/');
-	console.log('full_url = ', full_url)
 
-	// start redirecting if status allows and add '?noht=1' flag if needed
-	chrome.webNavigation.onDOMContentLoaded.addListener(function(tab) {
-		if (state) {
-			console.log('sbn: redirecting', tab.url, 'to', full_url);
-			chrome.tabs.update(tab.tabId, {url: full_url});
-//			if (tab.url.endsWith('?noht=1')) {
-//				console.log('sbn: redirecting', tab.url, 'to', new_url);
-//				chrome.tabs.update(tab.tabId, {url: new_url});
-//			} else {
-//				console.log('sbn: redirecting', tab.url, 'to', new_url+'?noht=1');
-//				chrome.tabs.update(tab.tabId, {url: new_url+'?noht=1'});
-//			}
-		}
-	}, {hostEquals: 'fastpic.ru', url: [{pathSuffix: s[7]}]});
+// check if host is 'fastpic.ru' and url ends with '.html'
+chrome.webNavigation.onBeforeNavigate.addListener(function(tab) {
+
+	// get the current url
+	var cur_url = tab.url;
+	console.log('cur_url:', cur_url);
+
+	// remove '.html' and change 'view' to 'fullview' in the url
+	var new_url = cur_url.replace('.html', '').replace('view', 'fullview');
+	console.log('new_url:', new_url);
+
+	// start redirecting if the status allows
+	if (state) {
+		console.log('sbn: redirecting', cur_url, 'to', new_url);
+		chrome.tabs.update(tab.tabId, {url: new_url});
+	}
+
 }, {url: [{hostEquals: 'fastpic.ru', urlSuffix: '.html'}]});
